ec463cb02486e87dc6d39a1debb9ac49ed9b888c
mdiaz88 <marcos.ad88@gmail.com>
2015-02-20 16:54:33 -0300

    javascript and jquery update- is not finished yet

--- practice/01-js-intro-and-jquery/Exercise1-javascript and jquery/js/scripts.js
+++ practice/01-js-intro-and-jquery/Exercise1-javascript and jquery/js/scripts.js
@@ -11,15 +11,33 @@ $('document').ready(function(){
 	$('.alias').css('border-color','red');
 
 	$("#acept").click(function(){
-        $.ajax({url: "http://bootcamp.aws.af.cm/welcome/yourname", success: function(result){
-            //$("#div1").html(result);
-            console.log(result);
-            var cambio=JSON.stringify(result);
-            alert(cambio);
-        }});
+        var name="Marcos";
+        $.ajax({url: "http://bootcamp.aws.af.cm/welcome/"+name, success: function(result){
+            var changetext=JSON.stringify(result);
+            var txt=changetext.substring(13,28);
+            $("#hidden").css('visibility','visible');
+            $("#newtext").text(txt);
+
+
+
+        }, error: function (xhr, ajaxOptions, thrownError) {
+            $("#hidden").css('visibility','visible');
+            $("#newtext").text("An error ocurred, status="+xhr.status);
+            $("#newtext").css('color','red');
+      }});
     });
 
 });
+
+/*this function isn´t working yet*/
+/*function hlight(result){
+    var hltxt1=result.substring(13,19);
+    var hltxt2=result.substring(21,27);
+    var className="highlighttext";
+    var hlresult="<span class=\"" + className + "\">" + hltxt2 + "</span>";
+    var total=hltxt1+" "+hlresult+"!";
+    $("#newtext").text(total);
+}*/
 
 
    
