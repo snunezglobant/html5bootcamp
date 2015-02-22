564395bd1ac202ef896744a14aa1b273084830fa
nicolegui10 <nicolegui77@gmail.com>
2015-02-22 08:17:10 -0300

    Problem about the 'response' resolved, and progress on the exercise 9 and 10

--- practice/01-js-intro-and-jquery/script.js
+++ practice/01-js-intro-and-jquery/script.js
@@ -1,28 +1,35 @@
 $(document).ready(function(){
 
-	$('section').fadeIn('slow', function(){
 
-			$('.alias').focus();
-		});
+	$('section').fadeIn(3000);
+
+	$('.alias').focus();
+
+	$("button").click( function() {
+		$.ajax('http://bootcamp.aws.af.cm/welcome/nicolas', {
+     	 success: function(result) {
+        $('section').html(result.response);
+      },
+      	 error: function(){
+
+      	 	$('section').css('font-color','red');
+
+      	 },
+         complete: function(){
+
+          $('section').highlight('nicolas');
 
 
-	/*
-	$('button').click( function(){
-		$.ajax('http://bootcamp.aws.af.cm/welcome/yourname',
-
-			success: function(response){
-				$('section').html(response).fadeIn();
-
-			},
-			error: function(){
-				$('section').css('font-color':'red');
 
 
-			}
-	});
-		
-	--------
-		I put this as a comment, because when I run it does not show me anything, not even what it shows me now. I would like to know where my mistake because I think the syntax is fine. Thank You
+           }});
+      });
 
-	*/
-});
+       $.ajax('https://api.spotify.com/v1/search',{data: "q=Rolling Stones&type=album"},
+        function(response){
+            console.log(response);
+        }
+
+       );
+
+});	
