01528b7aea57933de7dfc4c4cf8649830c540df1
nicolegui10 <nicolegui77@gmail.com>
2015-02-23 12:24:40 -0300

    All exercises about jquery complete

--- practice/01-js-intro-and-jquery/script.js
+++ practice/01-js-intro-and-jquery/script.js
@@ -5,31 +5,29 @@ $(document).ready(function(){
 
 	$('.alias').focus();
 
-	$("button").click( function() {
-		$.ajax('http://bootcamp.aws.af.cm/welcome/nicolas', {
-     	 success: function(result) {
-        $('section').html(result.response);
-      },
-      	 error: function(){
 
-      	 	$('section').css('font-color','red');
+      $("button").click(function(){  
+        var var1= $('.alias').val();
+        var var2= "album";
 
-      	 },
-         complete: function(){
+        $.ajax("https://api.spotify.com/v1/search",{data:{'q': var1, 'type': var2}})
+        .done(function(result){
+          $('#albumes').html("");
+          for(var i=0;i<(result.albums.items).length;i++){
+            $('#albumes').append("<li><p>Nombre del Album: "+result.albums.items[i].name+"<p>"
+              +"<p>Tipo: "+result.albums.items[i].album_type+"<p>"
+              +"<a href=\""+result.albums.items[i].external_urls.spotify+"\">"+"<p>Escucha aqui</p>"+"</a>"
+              +"<img src=\""+result.albums.items[i].images[0].url+"\"/>"+"<li/>")
+            
+          }
+        })
+        .fail(function(){
+            alert("Error!");
+        })
+        .always(function(){
+            console.log("Complete");
+        });
 
-          $('section').highlight('nicolas');
-
-
-
-
-           }});
       });
-
-       $.ajax('https://api.spotify.com/v1/search',{data: "q=Rolling Stones&type=album"},
-        function(response){
-            console.log(response);
-        }
-
-       );
 
 });	
