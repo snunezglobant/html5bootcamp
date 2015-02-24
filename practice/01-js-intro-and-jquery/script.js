66c6854b581766855c12f69487dc7b9ddd770153
nicolegui10 <nicolegui77@gmail.com>
2015-02-23 22:52:05 -0300

    Corrected erros applying your tips

--- practice/01-js-intro-and-jquery/script.js
+++ practice/01-js-intro-and-jquery/script.js
@@ -10,24 +10,34 @@ $(document).ready(function(){
         var var1= $('.alias').val();
         var var2= "album";
 
+        
+
         $.ajax("https://api.spotify.com/v1/search",{data:{'q': var1, 'type': var2}})
         .done(function(result){
-          $('#albumes').html("");
-          for(var i=0;i<(result.albums.items).length;i++){
-            $('#albumes').append("<li><p>Nombre del Album: "+result.albums.items[i].name+"<p>"
+          $('#albumes').empty();
+          
+          for(var i in result.albums.items){
+            var html = "<li><p>Nombre del Album: "+result.albums.items[i].name+"<p>"
               +"<p>Tipo: "+result.albums.items[i].album_type+"<p>"
               +"<a href=\""+result.albums.items[i].external_urls.spotify+"\">"+"<p>Escucha aqui</p>"+"</a>"
-              +"<img src=\""+result.albums.items[i].images[0].url+"\"/>"+"<li/>")
+              +"<img src=\""+result.albums.items[i].images[0].url+"\"/>"+"<li/>"
+
+              $('#albumes').append(html);
             
-          }
-        })
+                  }
+
+              })
+        
         .fail(function(){
+          
             alert("Error!");
-        })
+              })
         .always(function(){
             console.log("Complete");
-        });
+              });
 
+      
       });
 
-});	
+  
+  });	
