04e3f1403d59cdaa9211c4b9484120feaa685e0d
snunezglobant <sergio.nunez@globant.com>
2015-02-24 01:15:33 -0300

    Topic1: exersice 12. Now it shows all albums when a name is search.

--- practice/01-js-intro-and-jquery/script.js
+++ practice/01-js-intro-and-jquery/script.js
@@ -17,14 +17,16 @@ $("#buscar").click(function(){
 	var1=$("#nombre").val();
 	$.ajax( "https://api.spotify.com/v1/search",
  	{data: {q: var1, type: "album"}})
+
 .done(function(response){
-	$("#image").css("display","block");
-	$("#image").html("<img src=\""+response.albums.items[0].images[2].url+"\"/>");
-	$("#name").html(response.albums.items[0].name);
-	$("#type").html(response.albums.items[0].type);
-	$("#link").html(response.albums.items[0].external_urls.spotify);
-	$("#link").attr("href",response.albums.items[0].external_urls.spotify);
+	for (var i = 0; i <= 100; i++) {
+	$("#album").append("<p>\n" + "<img src=\""+response.albums.items[i].images[2].url+"\"/>"+ "</p>");
+	$("#album").append("<p>\n" +response.albums.items[i].name+ "</p>");
+	$("#album").append("<p>\n" +response.albums.items[i].type+ "</p>");
+	$("#album").append("<p>\n <a href=\"" +response.albums.items[i].external_urls.spotify+  "\"> Link to Album \n\n</a>");
+	$("#album").attr("href", response.albums.items[i].external_urls.spotify );
 	console.log(response);
+}
 })
 .fail(function(){
 	alert("error");
