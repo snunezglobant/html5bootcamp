35e95cf6d7a52c7b0167792dc6c1df4f546533e0
snunezglobant <sergio.nunez@globant.com>
2015-02-23 23:57:08 -0300

    Topic1: exercise 12 and some changes to the entire code

--- practice/01-js-intro-and-jquery/script.js
+++ practice/01-js-intro-and-jquery/script.js
@@ -1,28 +1,44 @@
 $(document).ready(function()
 {
-	
-/*$("section").fadeOut("slow", function(){$(".alias").focus()});*/
 
-$("button").click(function(){
+$("section").fadeOut("slow", function(){$(".alias").focus()});
+
+$("#clickme").click(function(){
 	$.ajax({ url: "http://bootcamp.aws.af.cm/welcome/" + $(".alias").val(), 
 			success:function(data){
 				console.log(data);
-			$("section").html("<p>"+data.response+"</p>");},
+			$("#welcome").html("<p>"+data.response+"</p>");},
 			error:function(data){ 
 				$("section").text("Error occurs"); 
 				$("section").css("font-color","red");}
 			});
 		});
-
-$.ajax( "https://api.spotify.com/v1/search",
- {data: {q: "rolling stones", type: "album"}})
+$("#buscar").click(function(){
+	var1=$("#nombre").val();
+	$.ajax( "https://api.spotify.com/v1/search",
+ 	{data: {q: var1, type: "album"}})
 .done(function(response){
+	$("#image").css("display","block");
 	$("#image").html("<img src=\""+response.albums.items[0].images[2].url+"\"/>");
 	$("#name").html(response.albums.items[0].name);
 	$("#type").html(response.albums.items[0].type);
 	$("#link").html(response.albums.items[0].external_urls.spotify);
 	$("#link").attr("href",response.albums.items[0].external_urls.spotify);
 	console.log(response);
+})
+.fail(function(){
+	alert("error");
+})
+.always(function(){
+	console.log("complete");
 });
 
 });
+});
+
+/*
+function highlight(res,name){
+	var str= res.replace(name, "<span class=\"highlight\">"+name+"</span>");
+	$("#newtext").html(str);
+}
+*/
