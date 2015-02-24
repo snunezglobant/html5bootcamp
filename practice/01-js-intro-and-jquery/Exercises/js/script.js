db37eb4701cba723d7b79fe132822c8ee8548e49
navarrojp <jp_navarro@live.com>
2015-02-24 02:24:19 -0300

    1. Topic 1: Exercises 10 to 13 finished

--- practice/01-js-intro-and-jquery/Exercises/js/script.js
+++ practice/01-js-intro-and-jquery/Exercises/js/script.js
@@ -3,7 +3,7 @@ $(document).ready( function() {
     $('.alias').focus();
   });
   $('#submit').click(getResponse);
-  //get spotify api response
+  $('#search').click(getAlbums);
 });
 
 function getResponse(){
@@ -17,7 +17,7 @@ function getResponse(){
       highlightName();
     })  
     .fail(function() {
-      $('section').html("<span id=\"error\">Sorry, server error!</span>");
+      $('section').html("<span class=\"error\">Sorry, server error!</span>");
     })
     .always(function(){
       $('section').removeClass('hidden');
@@ -31,4 +31,43 @@ function highlightName(){
   $('#greeting').html(highlighted);
 }
 
-//spotify api response
+//Exercise 11 - 12
+
+function getAlbums(){
+  var $artist = $('.artist').val();
+  var spotifyAPI = "https://api.spotify.com/v1/search";
+  var searchQuery = {q:$artist, type:'album'};
+  
+  $.getJSON (spotifyAPI, searchQuery)
+  .done(function( data ) {
+    console.log(data);
+    var albums = [];
+    //Here I'm saving each album in my array 'albums'
+    for (var i = 0 ; i < data.albums.items.length; i++) {
+      albums.push(data.albums.items[i]);
+    };
+    //Here I'm showing each album in the page
+    for(var i = 0; i < albums.length; i++){
+      
+      var img_url = albums[i].images[0].url;
+      var relDate = albums[i].release_date;
+      var type = albums[i].album_type;
+      var albumName = albums[i].name;
+      var stfyURL = albums[i].uri;
+      
+      $('#content').append("<div class=\"album-data\"></div>");
+
+      $('.album-data').append("<img class=\"album_img\" src=" + img_url + " alt=\"Album Image\" width=\"160\" height=\"160\">");
+      $('.album-data').append("<div class=\"albumName\">" + "Album Name: " + albumName + "</div>");
+      $('.album-data').append("<div class=\"release_date\">" + "Release Date: " + relDate + "</div>");
+      $('.album-data').append("<div class=\"album_type\">" + "Type: " + type + "</div>");
+      $('.album-data').append("<div class=\"spotify_url\">" + "Spotify URL: " + stfyURL + "</div>");
+      
+    };
+
+  })
+  .fail(function() {
+    $('.albums-container').append("<span class=\"error\">Sorry, an error ocurred!</span>");
+  })
+  
+}
