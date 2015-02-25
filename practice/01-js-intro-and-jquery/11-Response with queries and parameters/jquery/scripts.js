73a1edb5fa651795a5e7ddfa14d8783b3c98e37d
groncedo <gonzalo.roncedo08@gmail.com>
2015-02-25 14:13:42 -0200

    Made topic 1, exercise 11

---
+++ practice/01-js-intro-and-jquery/11-Response with queries and parameters/jquery/scripts.js
@@ -0,0 +1,38 @@
+var name = "Gonzalo";
+var album = "album";
+
+function visible() {
+   document.getElementById("section").style.visibility = "visible";
+}
+
+$('document').ready(function() {
+
+   $('.alias').focus();
+   $('.alias').css('border-color', 'red');
+
+    
+    $("#find").click(function(){
+        var artist=document.getElementById("txtsearch").value; 
+        $.ajax("https://api.spotify.com/v1/search",{data:{'q': artist, 'type': album}})
+        .done(function(result){
+            $("#metadata").css('display','block');
+            $("#metadata").html("<img src=\""+result.albums.items[0].images[0].url+"\"/>");
+            $("#artists").css('display','block');
+            $("#name").html(result.albums.items[0].name);
+            $("#atype").html(result.albums.items[0].album_type);
+            $("#type").html(result.albums.items[0].type);
+            $("#link").html(result.albums.items[0].external_urls.spotify);
+            $("#link").attr("href",result.albums.items[0].external_urls.spotify);
+            $("#content").css("visibility","visible");
+            console.log(result);
+        })
+        .fail(function(){
+            console.log("Error");
+        })
+        .always(function(){
+            console.log("Eitherway you'll get this");
+        });
+
+    });
+
+});
