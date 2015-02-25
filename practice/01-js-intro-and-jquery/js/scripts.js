dac82759db953e686f66d0cd1f541ceb09bfecd8
aepedraza <aepedraza3117@gmail.com>
2015-02-25 20:35:06 -0300

    Topic 1 complete

--- practice/01-js-intro-and-jquery/js/scripts.js
+++ practice/01-js-intro-and-jquery/js/scripts.js
@@ -27,24 +27,58 @@ $("#button").click (function(){
         });
 });
 
-/**************************/
-/*  Excercise 1.11  */
-var search = 'Rolling Stones';
-var type = 'album';
-var url = 'https://api.spotify.com/v1/search';
+/*************************/
+/*  Excercise 1.11 -1.12 */
+var searchAlbums = function(search){
+    console.log("Hello, I'm searching");
+    console.log(search);
+    var type = 'album';
+    var url = 'https://api.spotify.com/v1/search';
+    var imgSizeIndex = 2;
 
-$.ajax({
-    dataType: "json",
-    url: url,
-    data: {
-        q: search,
-        type: type
-    },
-    success: function(response){
-        console.log(response);
-        var albums = [];
-        $.each(response, function(album){
-            albums.push(album.items[0].name); // Don't know how to catch the items in the response
-        });
-    }
+    $.ajax({
+        dataType: "json",
+        url: url,
+        data: {
+            q: search,
+            type: type
+        },
+        // to show: name, type, image, release_date, and a link to spotify for that album
+        // THERE'S NOT ANY RELEASE DATE IN THE RESPONSE, SORRY
+        success: function(response){
+            var html = '';
+            //console.log(response); //For future debugging
+
+            for (var i = 0; i < response.albums.items.length; i++) {
+                var html = '';
+                html += '<tr><td><img src="'
+                        + response.albums.items[i].images[imgSizeIndex].url
+                        + '" width=' + response.albums.items[i].images[imgSizeIndex].width
+                        + ' height='+ response.albums.items[i].images[imgSizeIndex].height
+                        + '></td>';
+
+                html += '<td><p><b>Album Name:</b> '
+                        + response.albums.items[i].name
+                        + '</p>';
+
+                html += '<p><b>Type:</b> '
+                        + response.albums.items[i].album_type
+                        + '</p>';
+
+                html += '<p><b>link:</b> '
+                        + '<a href="'
+                        + response.albums.items[i].external_urls.spotify
+                        + '">Link to spotify</a>'
+                        + '</p></tr>';
+                $("#results").append(html);
+            };
+        }
+    });
+}
+
+$("#search-form").submit(function(event){
+    event.preventDefault();
+    var queryValue = $("#query").val();
+    searchAlbums(queryValue);
+
 });
