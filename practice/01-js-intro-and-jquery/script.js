446cfd31661ce08a9ca262c12b50f3a77f4672cc
nicolegui10 <nicolegui77@gmail.com>
2015-02-26 16:10:36 -0300

    Corrected Indentation

--- practice/01-js-intro-and-jquery/script.js
+++ practice/01-js-intro-and-jquery/script.js
@@ -1,43 +1,24 @@
 $(document).ready(function(){
-
-
 	$('section').fadeIn(3000);
-
-	$('.alias').focus();
-
-
-      $("button").click(function(){  
-        var var1= $('.alias').val();
-        var var2= "album";
-
-        
-
-        $.ajax("https://api.spotify.com/v1/search",{data:{'q': var1, 'type': var2}})
-        .done(function(result){
-          $('#albumes').empty();
-          
+  $('.alias').focus();
+  $("button").click(function(){  
+    var var1= $('.alias').val();
+    var var2= "album";
+    $.ajax("https://api.spotify.com/v1/search",{data:{'q': var1, 'type': var2}})
+      .done(function(result){
+        $('#albumes').empty();
           for(var i in result.albums.items){
             var html = "<li><p>Nombre del Album: "+result.albums.items[i].name+"<p>"
               +"<p>Tipo: "+result.albums.items[i].album_type+"<p>"
               +"<a href=\""+result.albums.items[i].external_urls.spotify+"\">"+"<p>Escucha aqui</p>"+"</a>"
-              +"<img src=\""+result.albums.items[i].images[0].url+"\"/>"+"<li/>"
-
-              $('#albumes').append(html);
-            
-                  }
-
-              })
-        
-        .fail(function(){
-          
-            alert("Error!");
-              })
-        .always(function(){
-            console.log("Complete");
-              });
-
-      
+              +"<img src=\""+result.albums.items[i].images[0].url+"\"/>"+"<li/>";
+            $('#albumes').append(html);}
       });
-
-  
-  });	
+      .fail(function(){
+        alert("Error!");
+      });
+      .always(function(){
+        console.log("Complete");
+      });
+  });
+});
