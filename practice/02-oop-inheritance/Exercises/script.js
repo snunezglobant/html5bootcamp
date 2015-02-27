0901bdbfc69118187ff4e8ec730518cdbee98ad8
navarrojp <jp_navarro@live.com>
2015-02-27 13:39:46 -0300

    Topic 2 Exercise 7 finished

--- practice/02-oop-inheritance/Exercises/script.js
+++ practice/02-oop-inheritance/Exercises/script.js
@@ -1,7 +1,9 @@
 /* Exercise 1 - Creating Movie() class */
 /* Modeling the Subject class - (in this case, 'Movie()' is the 'Subject' being observed) */
 
-function Movie() {
+/* Movie class is commented because down below was refactored as a Module - (Exercise 7) */
+
+/* function Movie() {
 	this.attributes = {
 		'title':'',
 		'director':'',
@@ -37,7 +39,42 @@ Movie.prototype.addObserver = function (observer) {
  Movie.prototype.notify = function ( event ) {
  	var event = event;
 	this.observer.update( this.attributes['title'], event);
-}; 
+}; */
+
+/* Exercise 7 - Refactor Movie class as Module keeping your previous code for reference */
+/* Refactoring Movie() as a Module */
+var Movie = {};
+
+var Movie = ( function () {
+	var attributes = {
+		'title': '',
+		'director': '',
+		'year': ''
+	};
+
+	var observer = new MovieObserver();
+
+	return {
+		set: function( attr, value) {
+			attributes[ attr ] = value;
+		},
+		get: function( attr ) {
+			return attributes[ attr ];
+		},
+		play: function( ) {
+			var event = "Playing";
+			this.notify(event);	
+		},
+		stop: function() {
+			var event = "Stopped";
+			this.notify(event);	
+		},
+		notify: function( event ) {
+			var event = event;
+			observer.update( attributes['title'], event);
+		}
+	};
+});
 
 /* Exercise 3 - Adding a MovieObserver class() */
 
@@ -52,10 +89,13 @@ function MovieObserver() {
 var inception = new Movie();
 inception.set("title", "Inception" );
 inception.set("director", "Christopher Nolan" );
+inception.set("year", "2010");
 
 var avatar = new Movie();
 avatar.set("title", "Avatar" );
 avatar.set("director", "James Cameron");
+avatar.set("year", "2009");
+
 
 /* Playing and Stopping */
 
@@ -64,3 +104,4 @@ avatar.play();
 
 inception.stop();
 avatar.stop();
+
