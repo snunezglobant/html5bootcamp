6929a8ffc5b108ea131dfdc78ccec70b2b682730
Franco Gallardo Grazio <franco.massa@hotmail.com>
2015-03-05 20:12:01 -0300

    Topic 5: MVC - AngularJS - Exercise 3

--- practice/05-angularjs/main.js
+++ practice/05-angularjs/main.js
@@ -9,6 +9,9 @@ moviesApp.config(['$routeProvider',function($routeProvider) {
     when('/details/:movieId', {
       templateUrl: 'templates/movie-details.html'
     }).
+    when('/edit/:movieId', {
+      templateUrl: 'templates/movie-edit.html'
+    }).
     otherwise({
       redirectTo: '/list'
     });
@@ -22,8 +25,12 @@ moviesApp.controller('MovieListCtrl', ['$scope', '$location', 'MovieStorage', fu
     $location.path('/details/' + item.guid);
   }
 
-  $scope.editBtnClick = function(item) {
-    console.log(item);
+  $scope.addItem = function() {
+    $location.path('/edit/new');
+  }
+
+  $scope.editItem = function(item) {
+    $location.path('/edit/' + item.guid);
   }
 
   $scope.removeItem = function(index, item) {
@@ -42,13 +49,38 @@ moviesApp.controller('MovieListCtrl', ['$scope', '$location', 'MovieStorage', fu
   }
 }]);
 
-moviesApp.controller('MovieDetailsCtrl', ['$scope', '$routeParams', 'MovieStorage', function($scope, $routeParams, MovieStorage){
-  $scope.movie = MovieStorage.loadMovie($routeParams.movieId);
-}]);
 
 /* localStorage persistance */
-moviesApp.service('MovieStorage', ['$http', 'guid', function($http, guid) {
+moviesApp.service('MovieStorage', ['$http', 'Movie', function($http, Movie) {
   var listPrefix = 'moviesApp-movie-';
+
+  this.newMovie = function() {
+    return new Movie();
+  }
+
+  this.save = function(item) {
+    var jsonIndex = localStorage.getItem(listPrefix + 'index');
+    var index = jsonIndex ? JSON.parse(jsonIndex) : [];
+
+    if (!localStorage.getItem(listPrefix + item.guid)) {
+      index.push(item.guid);
+      localStorage.setItem(listPrefix + 'index', JSON.stringify(index));
+    }
+
+    localStorage.setItem(listPrefix + item.guid, angular.toJson(item)); //JSON.stringify(item)
+  }
+
+  this.remove = function(item) {
+    var index = JSON.parse(localStorage.getItem(listPrefix + 'index'));
+
+    index.splice(index.indexOf(item.guid), 1);
+    localStorage.setItem(listPrefix + 'index', JSON.stringify(index));
+    localStorage.removeItem(listPrefix + item.guid);
+  }
+
+  this.loadMovie = function(guid) {
+    return JSON.parse(localStorage.getItem(listPrefix + guid));
+  }
 
   this.loadMovies = function() {
     var jsonIndex = localStorage.getItem(listPrefix + 'index');
@@ -67,27 +99,6 @@ moviesApp.service('MovieStorage', ['$http', 'guid', function($http, guid) {
     return items;
   }
 
-  this.loadMovie = function(guid) {
-    return JSON.parse(localStorage.getItem(listPrefix + guid));
-  }
-
-  this.save = function(item) {
-    var jsonIndex = localStorage.getItem(listPrefix + 'index');
-    var index = jsonIndex ? JSON.parse(jsonIndex) : [];
-
-    index.push(item.guid);
-    localStorage.setItem(listPrefix + 'index', JSON.stringify(index));
-    localStorage.setItem(listPrefix + item.guid, angular.toJson(item)); //JSON.stringify(item)
-  }
-
-  this.remove = function(item) {
-    var index = JSON.parse(localStorage.getItem(listPrefix + 'index'));
-
-    index.splice(index.indexOf(item.guid), 1);
-    localStorage.setItem(listPrefix + 'index', JSON.stringify(index));
-    localStorage.removeItem(listPrefix + item.guid);
-  }
-
   this.loadExampleMovieList = function() {
     var items = [];
     var self = this;
@@ -95,9 +106,14 @@ moviesApp.service('MovieStorage', ['$http', 'guid', function($http, guid) {
     $http.get('exampleMovieList.json', {responseType: 'json'})
       .success(function(data, status, headers, config) {
         for (var i = 0; i < data.length; i++) {
-          items[i] = data[i];
-          items[i].guid = guid.newGuid();
+          items[i] = new Movie();
+
+          for (var attr in data[i]) {
+            items[i][attr] = data[i][attr];
+          }
           self.save(items[i]);
+
+          console.log(items[i]);
         }
       });
 
@@ -106,10 +122,77 @@ moviesApp.service('MovieStorage', ['$http', 'guid', function($http, guid) {
   }
 }]);
 
-moviesApp.service('guid', function() {
+moviesApp.service('GUID', function() {
   this.newGuid = function() {
     return 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'.replace(/x/g, function() {
       return Math.floor(Math.random()*16).toString(16);
     });
   }
 });
+
+moviesApp.factory('Movie', ['GUID', function(GUID) {
+  function Movie() {
+    this.title = '';
+    this.imageUrl = '';
+    this.year = '';
+    this.genres = [''];
+    this.directors = [{
+      name: '',
+      url: ''
+    }];
+    this.stars = [{
+      name: '',
+      url: ''
+    }];
+    this.plot = '';
+
+    this.guid = GUID.newGuid();
+  }
+
+  return Movie;
+}]);
+
+
+/* 2) Show movie details in a separate details view. */
+moviesApp.controller('MovieDetailsCtrl', ['$scope', '$routeParams', '$location', 'MovieStorage', function($scope, $routeParams, $location, MovieStorage) {
+  $scope.movie = MovieStorage.loadMovie($routeParams.movieId);
+
+  $scope.back = function() {
+    $location.path('/list');
+  }
+}]);
+
+
+/* 3) Allow to add / edit / remove movies from the list. */
+moviesApp.controller('MovieEditCtrl', ['$scope', '$routeParams', '$location', 'MovieStorage', function($scope, $routeParams, $location, MovieStorage) {
+  if ($routeParams.movieId === 'new') {
+    $scope.movie = MovieStorage.newMovie();
+  }
+  else {
+    $scope.movie = MovieStorage.loadMovie($routeParams.movieId);
+  }
+
+  $scope.addGenre = function() {
+    $scope.movie.genres.push('');
+  }
+
+  $scope.addPerson = function(scope) {
+    scope.push({
+      name: '',
+      url: ''
+    });
+  }
+
+  $scope.removeItem = function(scope, index) {
+    scope.splice(index, 1);
+  }
+
+  $scope.save = function() {
+    MovieStorage.save($scope.movie);
+    $location.path('/list');
+  }
+
+  $scope.cancel = function() {
+    $location.path('/list');
+  }
+}]);
