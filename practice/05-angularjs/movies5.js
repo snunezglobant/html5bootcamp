369c801aedae967b3f5571f9bc29646417864663
Gamboster <gabrielmasclef@hotmail.com>
2015-03-05 22:33:45 -0300

    Angular - Topic 5: exercises 1, 2 and 3

--- practice/05-angularjs/movies5.js
+++ practice/05-angularjs/movies5.js
@@ -1,7 +1,47 @@
 
-
 function movieList($scope) {
-	$scope.movies = [{title: 'Terminator', year:'1984'},
-	{title: 'Forrest Gump', year:'1995'},
-	{title: 'The Shawshank Redemption', year:'1994'}];
+  $scope.movies = [{title: 'Terminator', year:'1984' , marked: false},
+  {title: 'Forrest Gump', year:'1995' , marked: false},
+  {title: 'The human centipede', year:'2010' , marked: false},
+  {title: 'The Good, the Bad and the Ugly', year:'1966' , marked: false},
+  {title: 'The Shawshank Redemption', year:'1994', marked: false}];
+
+  $scope.addMovie = function() {
+    $scope.movies.push({title: $scope.textNewMovieTitle,year: $scope.textNewMovieYear, marked: false});
+    $scope.textNewMovieTitle = '';
+    $scope.textNewMovieYear = '';
+  };
+
+  $scope.deleteMovie = function() {
+    var oldMovies = $scope.movies;
+    $scope.movies = [];
+    angular.forEach(oldMovies, function(movie) {
+      if (!movie.marked) $scope.movies.push(movie);
+    });
+  };
+
+
+}
+
+
+function ClickToEditCtrl($scope) {
+  $scope.title = $scope.movie.title;
+  $scope.year = $scope.movie.year;
+  $scope.editorEnabled = false;
+
+  $scope.enableEditor = function() {
+    $scope.editorEnabled = true;
+    $scope.editableTitle = $scope.movie.title;
+    $scope.editableYear = $scope.movie.year;
+  };
+
+  $scope.disableEditor = function() {
+    $scope.editorEnabled = false;
+  };
+
+  $scope.save = function() {
+    $scope.movie.title = $scope.editableTitle;
+    $scope.movie.year = $scope.editableYear;
+    $scope.disableEditor();
+  };
 }
