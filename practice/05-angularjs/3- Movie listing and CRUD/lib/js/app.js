3c40d7952d6c93e43f6ff1a0fda2c0671d5e9e26
groncedo <gonzalo.roncedo08@gmail.com>
2015-03-09 03:02:10 -0200

    Topic 4, Exercise 3 with data 'local storage'

---
+++ practice/05-angularjs/3- Movie listing and CRUD/lib/js/app.js
@@ -0,0 +1,56 @@
+var app = angular.module('aplication', []);
+app.controller('Movies', function($scope) {
+   $scope._id = null;
+   $scope.name = '';
+   $scope.plot = ''
+   $scope.year = '';
+   $scope.director = '';
+   $scope.genre = '';
+   $scope.movies = [];
+   $scope.saveMovie = function() {
+      if ($scope._id == null) {
+         $scope.movies.push({
+            name: $scope.name,
+            desc: $scope.desc,
+            year: $scope.year,
+            director: $scope.director,
+            genre: $scope.genre
+         }); 
+      } else {
+         $scope.movies[$scope._id] = {
+            name: $scope.name,
+            desc: $scope.desc,
+            year: $scope.year,
+            director: $scope.director,
+            genre: $scope.genre
+         };
+      }
+      $scope.cleanData();
+   }
+   $scope.changeMovies = function(index) {
+      $scope._id = index;
+      $scope.name = $scope.movies[index].name;
+      $scope.desc = $scope.movies[index].desc;
+      $scope.year = $scope.movies[index].year;
+      $scope.director = $scope.movies[index].director;
+      $scope.genre = $scope.movies[index].genre;
+   };
+   $scope.removeMovies = function(index) {
+      var movies_update = [];
+      for (var i = 0; i < $scope.movies.length; i++) {
+         if (i != index) {
+            movies_update.push($scope.movies[i]);
+         }
+      }
+      $scope.movies = movies_update;
+   };
+   $scope.cleanData = function() {
+      $scope._id = null;
+      $scope.name = '';
+      $scope.desc = '';
+      $scope.year = '';
+      $scope.director = '';
+      $scope.genre = '';
+      document.getElementsByClassName("name")focus();
+   };
+});
