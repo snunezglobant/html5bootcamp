7963587253f9cb1978aaafd2094f6a8f56b2ec24
navarrojp <jp_navarro@live.com>
2015-03-09 00:55:51 -0300

    Topic 5 Exercises 1 and 2 Refactored

--- practice/05-angularjs/Exercises/app.js
+++ practice/05-angularjs/Exercises/app.js
@@ -1,29 +1,17 @@
 var app = angular.module('movies', [ ]);
 
-app.controller('MovieCtrl', ['$scope', function($scope) {
-	// Controller magic!
-	$scope.movies = {};
+app.controller('MovieList', ['$scope', function($scope) { 
+	$scope.movies = [];
+	
+	$scope.addMovie = function() {
+		$scope.movies.push({
+			"title": $scope.title, 
+			"year": $scope.year,
+			"director": $scope.director
+		})		
+	};
 
-	$scope.movies.details = [{
-		/* Movies JSON */
-		"name": "Inception",
-		"year": "2010",
-		"director": "Christopher Nolan"
-	},{
-		"name": "Interstellar",
-		"year": "2014",
-		"director": "Christopher Nolan"
-	},{
-		"name": "Face Off",
-		"year": "1997",
-		"director": "John Woo"
-	},{
-		"name": "Shutter Island",
-		"year": "2010",
-		"director": "Martin Scorsese"		
-	}];
-
-	$scope.deleteMovie = function (index) {
-		$scope.movies.details.splice(index, 1)
+	$scope.removeMovie = function( index ) {
+		$scope.movies.splice(index, 1);
 	};
 }]);
